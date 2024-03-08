3829d993f5f0606328ccef1e7c3727844e1baa8b
Josh Robbins <35516944+BlackMirrorz@users.noreply.github.com>
2024-03-08 17:00:52 +0900

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "4.9.0"
+let checksum = "9109ce819eb9c62eafee160b274c74182430aae16774bcc54d4ad25aec9fddf2"
+
+let package = Package(
+    name: "TwinklOpenCV",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v12), .macCatalyst(.v13)
+    ],
+    products: [
+        .library(
+            name: "TwinklOpenCV",
+            targets: ["opencv2", "opencv2-dependencies"])
+    ],
+    targets: [
+        .binaryTarget(name: "opencv2",
+                      url: "https://github.com/yeatse/opencv-spm/releases/download/\(version)/opencv2.xcframework.zip",
+                      checksum: checksum),
+        .target(
+            name: "opencv2-dependencies",
+            linkerSettings: [
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("CoreImage"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreVideo", .when(platforms: [.iOS])),
+                .linkedFramework("Accelerate", .when(platforms: [.iOS, .macOS])),
+                .linkedFramework("OpenCL", .when(platforms: [.macOS])),
+                .linkedLibrary("c++")
+            ]
+        )
+    ]
+)
---
+++ Sources/opencv2-dependencies/Stub.swift
@@ -0,0 +1,8 @@
+//
+//  Stub.swift
+//  
+//
+//  Created by Josh Robbins on 3/8/24.
+//
+
+import Foundation
